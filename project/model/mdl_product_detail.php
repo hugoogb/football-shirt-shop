8ca7a45ad16c1f3666570efd31a1058f48e8c24d
Hugo GB <hugogaben8.02@gmail.com>
2022-11-18 08:11:39 +0100

    small change model

--- project/model/mdl_product_detail.php
+++ project/model/mdl_product_detail.php
@@ -2,11 +2,7 @@
 
 function getProductById($connDB, $id)
 {
-    if ($id) {
-        $query_result = pg_query_params($connDB, "SELECT * FROM products WHERE id = $1", array($id));
+    $query_result = pg_query_params($connDB, "SELECT * FROM products WHERE id = $1", array($id));
 
-        return pg_fetch_object($query_result);
-    }
-
-    return null;
+    return pg_fetch_object($query_result);
 }
