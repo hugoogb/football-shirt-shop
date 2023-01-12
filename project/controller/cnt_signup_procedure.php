9fe20f34bf4b93e3eda6ac6b99a03e9a8ff22542
Hugo GB <hugogaben8.02@gmail.com>
2023-01-12 18:29:04 +0100

    remove unused file and added condition

--- project/controller/cnt_signup_procedure.php
+++ project/controller/cnt_signup_procedure.php
@@ -2,15 +2,19 @@
 
 require_once __DIR__ . '/../model/mdl_connectDB.php';
 require_once __DIR__ . '/../model/mdl_signup.php';
+require_once __DIR__ . '/../model/mdl_validation.php';
 
 $connDB = connectDB();
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-$address = $_POST["address"];
-$city = $_POST["city"];
-$postal_code_zip = $_POST["postal-code-zip"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $address = $_POST["address"];
+    $city = $_POST["city"];
+    $postal_code_zip = $_POST["postal-code-zip"];
+}
+
 $values_register = [$name, $email, $password, $address, $city, $postal_code_zip];
 
 signUp($connDB, $values_register);
--- project/model/mdl_validation.php
+++ project/model/mdl_validation.php
@@ -6,7 +6,5 @@ function filter($values_register): void
     $isaddress = filter_var(·address, FILTER_SANITIZE_STRING_) != false;
     $iscity = filter_var(·city, FILTER_SANITIZE_STRING) != false;
     $ispostal_code_zip = filter_var(·postal_code_zip, FILTER_VALIDATE_int) != false;
-
-
 }
 
