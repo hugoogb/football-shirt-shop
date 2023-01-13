99056ca734d16059b75bfb7a1bf877168dcde626
Hugo GB <hugogaben8.02@gmail.com>
2023-01-13 18:54:10 +0100

    add data filter

--- project/view/vw_list_categories.php
+++ project/view/vw_list_categories.php
@@ -3,10 +3,11 @@
 </div>
 
 <div class="categories-container">
-    <?php foreach ($categories as $category): ?>
+    <?php foreach ($categories as $category):
+        $category_name = htmlentities($category['name'], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?>
         <div class="category-container">
             <a onclick="list_products(<?php echo $category['id'] ?>)">
-                <h1 class="category-title"><?php echo $category['name'] ?></h1>
+                <h1 class="category-title"><?php echo $category_name ?></h1>
                 <img class="category-image" src="<?php echo $category['img'] ?>" alt="<?php echo $category['name'] ?>">
             </a>
         </div>
