236f38958408e22014c0bf078f6518506207d7ab
Hugo GB <hugogaben8.02@gmail.com>
2023-01-20 20:24:41 +0100

    fixed bd connection error

--- project/model/mdl_connectDB.php
+++ project/model/mdl_connectDB.php
@@ -1,12 +1,6 @@
 <?php
 
-function connectDB(): ?\PgSql\Connection
+function connectDB()
 {
-    $result = pg_connect("host=localhost dbname=tdiw-k5 user=tdiw-k5 password=xmidd53M");
-
-    if (!$result) {
-        return null;
-    }
-
-    return $result;
+    return pg_connect("host=localhost dbname=tdiw-k5 user=tdiw-k5 password=xmidd53M");
 }
